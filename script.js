bd133639fcda7e11672940d0211d800b89700552
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-27 23:33:17 -0600

    Add socket, feather call to front end.

--- script.js
+++ script.js
@@ -18,3 +18,37 @@ window.addEventListener("DOMContentLoaded", async () => {
     console.error(err);
   }
 });
+
+// SOCKET SETUP
+const socket = io("http://localhost:6900");
+
+// FEATHERS SETUP
+const app = feathers();
+
+// THIS ENABLES SOCKET.IO TO SPEAK TO THE SERVER
+app.configure(feathers.socketio(socket));
+
+// SUBMIT LISTENER
+document.getElementById("form").addEventListener("submit", submitIdea);
+
+async function submitIdea(e) {
+  console.log("submit triggered");
+  e.preventDefault();
+
+  //   grab data from form
+  let text = document.getElementById("idea-text").value;
+  let tech = document.getElementById("idea-tech").value;
+  let viewer = document.getElementById("idea-viewer").value;
+
+  //   Create new Idea in Feathers
+  //   Note: The time stamping occurs on the backend
+  app.service("ideas").create({
+    text,
+    tech,
+    viewer
+  });
+
+  text = "";
+  tech = "";
+  viewer = "";
+}
